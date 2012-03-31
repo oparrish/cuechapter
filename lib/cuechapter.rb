d8ab7f1f9b478badbacbd6c56e9f8d37f5f1423c
Owen Parrish <owen.parrish@gmail.com>
2012-03-30 22:42:42 -0400

    Always output to XML, guess the file name if not provided

--- lib/cuechapter.rb
+++ lib/cuechapter.rb
@@ -3,24 +3,25 @@ require 'builder'
 
 module CueChapter
   class CueChapter 
-    def initialize(cue_file, xml_file, image_file, link)
+    def initialize(cue_file, xml_file=nil, image_file=nil, link=nil)
       @cuesheet = RubyCue::Cuesheet.new(File.read(cue_file))
       @cuesheet.parse!
       
-      @xml_file = xml_file
+      if @xml_file.nil?
+      	@xml_file=File.basename(cue_file).gsub(File.extname(cue_file),".xml")
+      else
+      	@xml_file = xml_file
+      end
+      
       @image_file = image_file
       @link = link
     end
 
     def convert
-      if xml_file.nil?
-        puts chapter_xml
-      else
-        File.open(@options['xml'], 'w') { |f| f.write(chapter_xml) }
-      end
+    	File.open(@xml_file, 'w') { |f| f.write(to_chapter_xml) }
     end
-    
-    def chapter_xml
+      
+    def to_chapter_xml
       xml = Builder::XmlMarkup.new(:indent => 2)
 
       xml.chapters("version" => "1") {
