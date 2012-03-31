a02ab892e59c79f64772bd02ca8da149622cb5b7
Owen Parrish <owen.parrish@gmail.com>
2012-03-30 23:57:49 -0400

    Tests actually work now!

--- test/test_cuechapter.rb
+++ test/test_cuechapter.rb
@@ -3,9 +3,13 @@ require 'cuechapter'
 require 'rexml/document'
 
 class CueChapterTest < MiniTest::Unit::TestCase
+  def load_cuesheet(cuename)
+  	File.read(File.join(File.dirname(__FILE__), "fixtures/#{cuename}.cue"))
+	end
+  
   def setup
-    @cue_file = './fixtures/test.cue'
-    @xml_file = './fixtures/test.xml'
+    @cue_file = File.join(File.dirname(__FILE__), '/fixtures/test.cue')
+    @xml_file = File.join(File.dirname(__FILE__), '/fixtures/test.xml')
     @cuechapter = CueChapter::CueChapter.new(@cue_file, @xml_file)
   end
   
@@ -16,13 +20,19 @@ class CueChapterTest < MiniTest::Unit::TestCase
   	xml = REXML::Document.new file
   	
   	titles=[]
+  	starttimes=[]
   	
   	xml.elements.each("chapters/chapter/title") do |title|
   		titles << title.text
   	end
+  	
+  	xml.elements.each("chapters/chapter") do |chapter|
+  		starttimes << chapter.attributes["starttime"]
+  	end
     	
   	@cuechapter.songs.each_with_index do |song, i|
   		assert_equal "#{song[:performer]} - #{song[:title]}", titles[i]
+  		assert_equal "#{song[:index].minutes}:#{song[:index].seconds}", starttimes[i]
   	end
   end
   
